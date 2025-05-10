7dce80f1dee277b383033baf71f80ab7d37a4478
Masoodkhan501 <141975307+Masoodkhan501@users.noreply.github.com>
2025-05-10 22:50:28 +0530

    changes in the doctor class is done

--- src/main/java/com/masood/model/Doctor.java
+++ src/main/java/com/masood/model/Doctor.java
@@ -1,10 +1,99 @@
 package com.masood.model;
 
-public class Doctor 
-{
+import java.util.List;
+
+import org.springframework.stereotype.Component;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.JoinTable;
+import jakarta.persistence.ManyToMany;
+import jakarta.persistence.OneToOne;
+import jakarta.persistence.Table;
+
+@Entity(name = "Doctor")
+@Table(name = "Doctor_table")
+@Component
+public class Doctor {
+	@Id
+	@Column(name = "doc_id")
 	private String doc_id;
-	private Long user_id;
-	private Specialized specialized_at;
+	
+	@OneToOne
+	@JoinColumn(name = "user_Id")
+	private User user_id;
+	
+	@ManyToMany
+	@JoinTable(name = "Doctor_specialization", 
+	joinColumns = @JoinColumn(name = "Doctor_id"), 
+	inverseJoinColumns = @JoinColumn(name = "specialization_id"))
+	private List<Specialized> specializations;
+	
+	@Column(name = "years_of_experience")
 	private byte expreince;
+	
+	@Enumerated(EnumType.STRING)
+	@Column(name = "doc_status")
 	private Status status;
+
+	public Doctor() {
+	}
+
+	public Doctor(String doc_id, User user_id, List<Specialized> specializations, byte expreince, Status status) {
+		this.doc_id = doc_id;
+		this.user_id = user_id;
+		this.specializations = specializations;
+		this.expreince = expreince;
+		this.status = status;
+	}
+
+	public String getDoc_id() {
+		return doc_id;
+	}
+
+	public void setDoc_id(String doc_id) {
+		this.doc_id = doc_id;
+	}
+
+	public User getUser_id() {
+		return user_id;
+	}
+
+	public void setUser_id(User user_id) {
+		this.user_id = user_id;
+	}
+
+	public List<Specialized> getSpecialized_at() {
+		return specializations;
+	}
+
+	public void setSpecialized_at(List<Specialized> specialized_at) {
+		this.specializations = specialized_at;
+	}
+
+	public byte getExpreince() {
+		return expreince;
+	}
+
+	public void setExpreince(byte expreince) {
+		this.expreince = expreince;
+	}
+
+	public Status getStatus() {
+		return status;
+	}
+
+	public void setStatus(Status status) {
+		this.status = status;
+	}
+
+	public String toString() {
+		return "Doctor [doc_id=" + doc_id + ", user_id=" + user_id + ", specialized_at=" + specializations
+				+ ", expreince=" + expreince + ", status=" + status + "]";
+	}
+
 }
