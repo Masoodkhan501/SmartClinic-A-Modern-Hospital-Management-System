3473a9f64501783cf9d9fdbf35751e4806250f28
Masoodkhan501 <141975307+Masoodkhan501@users.noreply.github.com>
2025-05-12 11:00:13 +0530

    priscription is added

---
+++ src/main/java/com/masood/repository/PriscriptionRepo.java
@@ -0,0 +1,10 @@
+package com.masood.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.masood.model.priscription;
+
+public interface PriscriptionRepo extends JpaRepository<priscription, Long> 
+{
+
+}
