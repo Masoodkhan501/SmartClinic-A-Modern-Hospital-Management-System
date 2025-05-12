d89dd11ea0f798c97c1e99a6eef07517ac44f2f8
Masoodkhan501 <141975307+Masoodkhan501@users.noreply.github.com>
2025-05-12 11:02:11 +0530

    specialized repo is added

---
+++ src/main/java/com/masood/repository/SpecilizedRepo.java
@@ -0,0 +1,10 @@
+package com.masood.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.masood.model.Specialized;
+
+public interface SpecilizedRepo extends JpaRepository<Specialized, String> 
+{
+
+}
