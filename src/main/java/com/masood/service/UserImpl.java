6b4044211a7bbf163de9e30081316ab2d06404c8
Masoodkhan501 <141975307+Masoodkhan501@users.noreply.github.com>
2025-05-13 09:42:19 +0530

    user service mistake has been solved

--- src/main/java/com/masood/service/UserImpl.java
+++ src/main/java/com/masood/service/UserImpl.java
@@ -4,10 +4,12 @@ import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
 import com.masood.model.User;
 import com.masood.repository.UserRepo;
 
+@Service("userImplService")
 public class UserImpl implements UserSeviceInterface {
 
 	@Autowired
